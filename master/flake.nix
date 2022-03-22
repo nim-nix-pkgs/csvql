4e387ee217747d926a08476e45b5dc572d815981
hugosenari <hugosenari@gmail.com>
2022-03-22 00:27:04 -0300

    chore: re-index ../nimpkgs/c/csvql/master

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''csvql.'';
+  description = ''csvql - query csv using ansisql.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,13 +9,14 @@
   
   inputs.src-csvql-master.flake = false;
   inputs.src-csvql-master.owner = "Bennyelg";
-  inputs.src-csvql-master.ref   = "refs/heads/master";
+  inputs.src-csvql-master.ref   = "master";
   inputs.src-csvql-master.repo  = "csvql";
   inputs.src-csvql-master.type  = "github";
   
   inputs."cligen".owner = "nim-nix-pkgs";
   inputs."cligen".ref   = "master";
   inputs."cligen".repo  = "cligen";
+  inputs."cligen".dir   = "v1_5_22";
   inputs."cligen".type  = "github";
   inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
   inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
